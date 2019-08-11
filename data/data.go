c65aaebdc13e1b7c346427eecc7a3e0a58178811
Rodneystar <Rodneystar66@gmail.com>
2019-08-11 21:27:10 +0100

    fixing io on saves/reads

--- data/data.go
+++ data/data.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"time"
 )
 
@@ -11,14 +12,60 @@ type TimerEventDescription struct {
 }
 
 type DataAccess interface {
-	GetAllTimers() []TimerEventDescription
-	AddTimer(TimerEventDescription) bool
+	GetAllTimers() ([]TimerEventDescription, error)
+	AddTimer(TimerEventDescription) error
 }
 
-func toJson(event TimerEventDescription) ([]byte, error) {
+func NewDataAccess() DataAccess {
+	return &FileAccess{
+		filename: "timers.dat",
+	}
+}
+
+type FileAccess struct {
+	filename string
+}
+
+func (da *FileAccess) GetAllTimers() ([]TimerEventDescription, error) {
+	jsonIn, err := ioutil.ReadFile(da.filename)
+	if err != nil {
+		return nil, err
+	}
+	return fromJSONArr(jsonIn), nil
+}
+
+func (da *FileAccess) AddTimer(t TimerEventDescription) error {
+	currTimers, err := da.GetAllTimers()
+	if err != nil {
+		return err
+	}
+
+	currTimers = append(currTimers, t)
+	encodedTimers, err := toJSONArr(currTimers)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(da.filename, encodedTimers, 0644)
+}
+
+func toJSON(event TimerEventDescription) ([]byte, error) {
 	bytes, err := json.MarshalIndent(event, "", "\t")
 	if err != nil {
 		return nil, err
 	}
 	return bytes, nil
+}
+
+func toJSONArr(event []TimerEventDescription) ([]byte, error) {
+	bytes, err := json.MarshalIndent(event, "", "\t")
+	if err != nil {
+		return nil, err
+	}
+	return bytes, nil
+}
+
+func fromJSONArr(jsonArr []byte) []TimerEventDescription {
+	var timerEvent []TimerEventDescription
+	json.Unmarshal(jsonArr, &timerEvent)
+	return timerEvent
 }
--- data/data_test.go
+++ data/data_test.go
@@ -1,11 +1,56 @@
 package data
 
 import (
-	"fmt"
+	"io/ioutil"
+
 	"testing"
+	"time"
 )
 
 func Test_getAllTimers(t *testing.T) {
-	fmt.Println("first test")
+	now := time.Now()
+	event := TimerEventDescription{
+		AtTime: now,
+		State:  true,
+	}
+
+	event2 := TimerEventDescription{
+		AtTime: now.Add(-time.Hour * 2),
+		State:  false,
+	}
+	events := []TimerEventDescription{event, event2}
+	_ = events
+	data := NewDataAccess()
+
+	data.AddTimer(event)
+	allTimers, _ := data.GetAllTimers()
+	result := allTimers[0].AtTime
+	if result != now {
+		t.Errorf("expected %s, was: %s", now, result)
+	}
+
+}
+
+func Test_files(t *testing.T) {
+	now := time.Now()
+	event := TimerEventDescription{
+		AtTime: now,
+		State:  true,
+	}
+	arr := []TimerEventDescription{event}
+
+	jsonEvent, _ := toJSONArr(arr)
+	ioutil.WriteFile("timers.dat", jsonEvent, 0644)
+
+}
+
+func Test_readFiles(t *testing.T) {
+	jsonIn, err := ioutil.ReadFile("timers.dat")
+	if err != nil {
+		t.Error("read error, ", err)
+	}
+
+	timerEvents := fromJSONArr(jsonIn)
+	t.Logf("time 1: %s, action1: %t", timerEvents[0].AtTime, timerEvents[0].State)
 
 }
