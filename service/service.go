499ec51d9662f804f48c281445c891eeebf243a1
localadmin <localadmin@mac-14883.allstate.com>
2019-08-12 17:27:08 +0100

    refactored schedule, added service package

--- service/service.go
+++ service/service.go
@@ -1,1 +1,16 @@
-package service
+package main
+
+import (
+	"go_schedule/data"
+)
+
+type TimerService struct {
+	timers 						[]data.TimerEventDescription
+	runningTimerStopChannels 	[]chan bool
+}
+
+
+
+func main() {
+
+}
